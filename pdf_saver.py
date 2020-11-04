db5d17595970238d5faac3ffbcf56b3bfa134efb
cyl628 <55496186+cyl628@users.noreply.github.com>
2020-11-04 20:58:28 +0800

    Add files via upload

---
+++ pdf_saver.py
@@ -0,0 +1,69 @@
+  
+  
+import pdfkit  
+import requests  
+import datetime 
+import eventlet
+import time
+import os
+from multiprocessing import  Process
+
+path_wkthmltopdf = r'wkhtmltopdf.exe'
+#path_wkthmltopdf = r'/usr/local/bin/wkhtmltopdf'
+config = pdfkit.configuration(wkhtmltopdf = path_wkthmltopdf)
+
+def main(urlitemlist, index):
+    print('进程{}启动'.format(index))
+    failurl = []
+    for urlitem in urlitemlist:
+        l = urlitem.strip().split('\t')
+        filename = l[0]
+        filename_list = filename.split('-')
+        url = l[1]
+        dirname = './data/'+'/'.join([filename_list[0], filename_list[2]])
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        eventlet.monkey_patch()
+        try:
+            with eventlet.Timeout(30, True):
+                print_topdf(url, filename, dirname)
+        except: #eventlet.timeout.Timeout:
+            print('[FAIL]:', urlitem)
+            failurl.append(urlitem)
+    with open('failurl{}.txt'.format(index), 'w')as f:
+        f.writelines(''.join(failurl))
+    print('进程{}完成'.format(index))
+
+def print_topdf(url, filename, dirname):
+    curr_time = datetime.datetime.now().date()
+    date = curr_time.strftime("%Y/%m/%d") 
+    #print(date)
+    options = {
+        'page-size': 'A4',
+        'margin-top': '10mm',
+        'margin-right': '10mm',
+        'margin-bottom': '10mm',
+        'margin-left': '10mm',
+        'encoding': "UTF-8",
+        'no-outline': None,
+        'header-left':date,
+        'header-font-size':12,
+        'lowquality':''
+    }
+    pdfpath = os.path.join(dirname, filename)+'.pdf'
+    pdfkit.from_url(url, pdfpath, options = options, configuration = config)
+
+if __name__ == '__main__': 
+    with open('htmlpdf_url.txt', encoding='utf-8')as f:
+        lines = f.readlines()
+    lines = lines[:20]
+    num = int(len(lines) / 8) + 1
+    process_list = []
+    for i in range(8):  #开启5个子进程执行fun1函数
+        p = Process(target=main,args=(lines[i*num:(i+1)*num], i, )) #实例化进程对象
+        p.start()
+        process_list.append(p)
+    for i in process_list:
+        p.join()
+
+    print('结束')
---
+++ url_generator.py
@@ -0,0 +1,63 @@
+'''
+this file generate all urls to be saved as pdf file
+input:
+a table of companies and locations, with form:
+    xxx公司   xx省;xx市
+a table of base url for each location and each website
+'''
+
+
+# %%
+import pandas as pd
+import pickle
+
+print('读入机构列表...')
+df = pd.read_csv('company.txt', sep='\t')
+com2addr = dict(zip(list(df['公司']), list(df['省市'])))
+len(com2addr)
+# %%
+#生成字典 {xx公司:[xx省, xx市]}
+com2addr_new = {}
+for com in com2addr:
+    l =  com2addr[com].split(';')
+    l_new = []
+    l_new.append(l[0][:3])
+    if '赣江' in l[1]:
+        l_new.append('赣江新区')
+    else:
+        l_new.append(l[1][:3])
+    com2addr_new[com] = l_new
+print('一共{}个机构'.format(len(com2addr_new)))
+with open('com2addr.pkl', 'wb')as f:
+    pickle.dump(com2addr_new, f)
+print('完成')
+
+print('读入url列表...')
+# %%
+#生成字典 {xx省:{工商:base_url, 国土:base_url, ...}}
+urldic = {}
+urldf = pd.read_csv('url_list.txt', sep='\t')
+for item in urldf.iterrows():
+    #print(type(item[1]))
+    urldic[item[1]['省市']] = dict(zip(list(item[1].keys())[1:], list(item[1][1:])))
+print('一共{}个省市'.format(len(urldic)))
+print('完成')
+
+# %%
+#生成最终列表
+#九江银行股份有限公司-江西省-工商  url
+print('生成url列表...')
+with open('com2addr.pkl', 'rb')as f:
+    com2addr = pickle.load(f)
+linestowrite = []
+for com in com2addr:
+    for addr in com2addr[com]:
+        urls = urldic[addr]
+        for typename in urls:
+            if str(urls[typename]).startswith('http'):
+                url = urls[typename].replace('{}', com)
+                linestowrite.append('{}-{}-{}\t{}'.format(com, addr, typename, url))
+with open('htmlpdf_url.txt', 'w', encoding='utf-8')as f:
+    f.writelines('\n'.join(linestowrite))
+print('完成，生成文件：{}'.format('htmlpdf_url.txt'))
+# %%
